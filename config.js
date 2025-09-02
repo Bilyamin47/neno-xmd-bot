5501287cb3fd83c71d9dfe0cc87abc4aae35e120
Bilyamin47 <153896367+Bilyamin47@users.noreply.github.com>
2025-09-02 11:05:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "PUT YOUER SESON ID WITH OUT NENO-xmd~",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "NENO-XMD~eO5kEQ7D#39x3gYFi8ZCHauHAO18YLjHU0ZCaPssAc_zkaxsUFmk",
+  OWNER_NUM: process.env.OWNER_NUM || "255789587448",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
